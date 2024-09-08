ad0432f700b4334053bd4949a38049da02ecaefc
Sar1707 <sarntse@gmail.com>
2024-09-08 21:28:16 +0530

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -194,20 +194,7 @@ function Home() {
       </div>
     );
   
-  return (
-    <div className="w-full py-8 bg-white">
-      <Container>
-        <div className="flex flex-wrap">
-          {posts.map((post) => (
-            <div key={post.$id} className="p-2 w-1/4">
-              <PostCard {...post} />
-            </div>
-          ))}
-        </div>
-      </Container>
-    </div>
-    // this looks not needed
-  );
+  
 }
 
 export default Home;
